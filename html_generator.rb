f90f865e8d5f71d0bc97d19e2ce4857cd699d8ee
blibotics <blibotics@gmail.com>
2013-06-14 19:57:44 -0400

    First method, 'feeling_guilty' completed

--- html_generator.rb
+++ html_generator.rb
@@ -1,5 +1,38 @@
-class HtmlGenerator
 	require 'open-uri'
 	require 'json'
+class HtmlGenerator
+	#gathered raw data, aka a giant string
+	def feeling_guilty
+	raw_data = open("http://lcboapi.com/products?q=ontario").read
+	ontario_drinks = JSON.parse(raw_data)
+	#ontario_drinks - is an array of hashes
+	print_header
+
+	# ontario_drinks["result"]
+	ontario_drinks["result"].each do |attribute|
+		if attribute["price_in_cents"] >= 1000
+			puts "				<h1> #{attribute["name"]} </h1>"
+			puts "				<h4> $#{attribute["price_in_cents"]/100.to_f} <h4>"
+			puts "				<h4> #{((attribute["price_in_cents"]/100.to_f)/WATER).to_i} people<h4>"
+		else
+			next
+		end
+	print_end
+	end
+
+
+
+	def print_header
+		puts "<!DOCTYPE html>"
+		puts "<html>"
+		puts "		<head></head>"
+		puts "		<body>"
+	end
+
+	def print_end
+		puts "		<body>"
+		puts "</html>"
+	end
+
 
 end
