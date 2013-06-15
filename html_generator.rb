22ff40989b13daab7e19f192267d388c9fe5ff26
blibotics <blibotics@gmail.com>
2013-06-14 22:07:17 -0400

    Added new methods and updated router file

--- html_generator.rb
+++ html_generator.rb
@@ -1,9 +1,22 @@
 	require 'open-uri'
 	require 'json'
-	
+
 class HtmlGenerator
+	# cost to help get clean water to those in need in Africa http://waterwellsforafrica.org/whats-the-cost/
 	WATER = 3.5
-	
+
+	def print_header
+		puts "<!DOCTYPE html>"
+		puts "<html>"
+		puts "		<head></head>"
+		puts "		<body>"
+	end
+
+	def print_end
+		puts "		</body>"
+		puts "</html>"
+	end
+
 	def feeling_guilty
 	raw_data = open("http://lcboapi.com/products?q=ontario").read
 	ontario_drinks = JSON.parse(raw_data)
@@ -22,18 +35,53 @@ class HtmlGenerator
 		print_end
 	end
 
+	def random_drink
+		raw_data = open("http://lcboapi.com/products").read
+		random_drinks = JSON.parse(raw_data)
 
+		print_header
 
-	def print_header
-		puts "<!DOCTYPE html>"
-		puts "<html>"
-		puts "		<head></head>"
-		puts "		<body>"
+		random_drink = random_drinks["result"].sample
+			puts "				<img src='#{random_drink["image_url"]}' style='height:200px; width:200px;'/>"
+			puts "				<h1> Name: #{random_drink["name"]} </h1>"
+			puts "				<h4> Package: #{random_drink["package"]} </h4>"
+			puts "				<h4> Total package units: #{random_drink["total_package_units"]} </h4>"
+			puts "				<h4> Price: $#{random_drink["price_in_cents"]/100.to_f} </h4>"
+
+		print_end
 	end
 
-	def print_end
-		puts "		</body>"
-		puts "</html>"
+
+	def pick_type(type)
+		raw_data = open("http://lcboapi.com/products?q=#{type}").read
+		type_drinks = JSON.parse(raw_data)
+
+		print_header
+
+		type_drinks["result"].each do |attribute|
+			puts "				<h1> Name: #{attribute["name"]} </h1>"
+			puts "				<h4> Package: #{attribute["package"]} </h4>"
+			puts "				<h4> Total package units: #{attribute["total_package_units"]} </h4>"
+			puts "				<h4> Price: #{attribute["price_in_cents"]/100.to_f} </h4>"
+			puts "				<h4> Area: #{attribute["origin"]} </h4>"
+
+		print_end
+		end
+	end
+
+	def help
+		puts ""
+		puts "Need help with the commands? No problemo!"
+		puts "That's why I'm here brejo! (that's a soft 'j' btw)"
+		puts "--------------------------------------------------------------"
+		puts "--------------------------------------------------------------"
+		puts ""
+		puts "'guilty-time' ................ Feel bad for buying alcohol"
+		puts "'random-drink' ............... Need help picking a drink? Try this!"
+		puts "'pick-type' + _word_ ......... Have a specific query? Enter your word after 'pick-type'"
+		puts "'help' ....................... If you made it here, you already know this"
+		puts ""
+		puts ""
 	end
 
 end
--- router.rb
+++ router.rb
@@ -6,7 +6,17 @@ generator = HtmlGenerator.new
 if request == "guilty-time"
 	generator.feeling_guilty
 
+elsif request == "random-drink"
+	generator.random_drink
+
+elsif request == "pick-type"
+	search_text = ARGV[1]
+	generator.pick_type(search_text)
+
+elsif request == "help"
+	generator.help
+
 else
-	puts "Sorry, I do not compute."
+	puts "Sorry, I can not compute."
 end
 
